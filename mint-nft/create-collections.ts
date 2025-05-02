8aed4dc19d12c440f5b34c640d7d95a51080646d
Thomas Hoang <megatunger@gmail.com>
2025-05-02 14:58:13 +0700

    feat: update NFT collection generation process
    Modifies the collection generation to use a JSON URI for offchain metadata. Simplifies the image upload process by integrating the collection image URL directly in the JSON. Ensures a smoother minting process with improved logging for the generated NFT.

--- mint-nft/create-collections.ts
+++ mint-nft/create-collections.ts
@@ -8,16 +8,11 @@ import {
   createSignerFromKeypair,
   generateSigner,
   keypairIdentity,
-  KeypairSigner,
   percentAmount,
-  sol,
 } from "@metaplex-foundation/umi";
 import { mockStorage } from "@metaplex-foundation/umi-storage-mock";
-
-import { getExplorerLink } from "@solana-developers/helpers";
 import { promises as fs } from "fs";
-import * as path from "path";
-import { encryptVoucherData } from "./utils";
+import { getExplorerLink } from "@solana-developers/helpers";
 
 const secret = require("../secrets/wallet.json");
 
@@ -34,45 +29,29 @@ umi.use(mplTokenMetadata());
 umi.use(mockStorage());
 
 const generateCollection = async (brand: string) => {
-  const collectionImagePath = `./uploads/${brand}/collection.jpg`;
+  const jsonUri = `https://raw.githubusercontent.com/megatunger/omni-points/refs/heads/main/mint-nft/uploads/${brand}/collection.json`;
   const collectionData = require(`./uploads/${brand}/collection.json`);
+  // generate mint keypair
+  const collectionMint = generateSigner(umi);
 
-  const buffer = await fs.readFile(collectionImagePath);
-  let file = createGenericFile(buffer, collectionImagePath, {
-    contentType: "image/png",
-  });
-  const [image] = await umi.uploader.upload([file]);
-  console.log("image uri:", image);
+  // create and mint NFT
+  await createNft(umi, {
+    mint: collectionMint,
+    name: collectionData.name,
+    uri: jsonUri,
+    updateAuthority: umi.identity.publicKey,
+    sellerFeeBasisPoints: percentAmount(0),
+    isCollection: true,
+  }).sendAndConfirm(umi, { send: { commitment: "finalized" } });
 
-  const uri = await umi.uploader.uploadJson({
-    ...collectionData,
-    image,
-  });
-
-  console.log("Collection offchain metadata URI:", uri);
-
-  // // generate mint keypair
-  // const collectionMint = generateSigner(umi);
-  //
-  // // create and mint NFT
-  // await createNft(umi, {
-  //   mint: collectionMint,
-  //   name: collectionData.name,
-  //   uri,
-  //   updateAuthority: umi.identity.publicKey,
-  //   sellerFeeBasisPoints: percentAmount(0),
-  //   isCollection: true,
-  // }).sendAndConfirm(umi, { send: { commitment: "finalized" } });
-  //
-  // let explorerLink = getExplorerLink(
-  //   "address",
-  //   collectionMint.publicKey,
-  //   "devnet",
-  // );
-  // console.log(`Collection NFT:  ${explorerLink}`);
-  // console.log(`Collection NFT address is:`, collectionMint.publicKey);
-  // console.log("✅ Finished successfully!");
-  return uri;
+  let explorerLink = getExplorerLink(
+    "address",
+    collectionMint.publicKey,
+    "devnet",
+  );
+  console.log(`Collection NFT:  ${explorerLink}`);
+  console.log(`Collection NFT address is:`, collectionMint.publicKey);
+  console.log("✅ Finished successfully!");
 };
 
 generateCollection("vietjet");
